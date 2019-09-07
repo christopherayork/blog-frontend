c855dc61480ccf44d64047a36492fa3baa29de40
Chris York <christopherayork1992@gmail.com>
2019-09-07 05:04:23 -0400

    Project setup and initial organization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is where it all begins
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/styled-components/Home.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const HomeStyles = styled.div`
+
+`;
+
+export default HomeStyles
